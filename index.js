f217525e4b8bc3cf1647e5b6c15f37fd2f7fb9e4
Omar Aref <36766407+OmarKnows@users.noreply.github.com>
2021-06-07 04:20:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,18 @@ const app = express();
 
 connect();
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', '*');
+
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+});
+
 app.use(bodyParser.json());
 
 app.use('/', router);
